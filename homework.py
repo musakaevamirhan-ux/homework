1e71a69932b0c940bfa687ace89a27564247d880
musakaevamirhan-ux <musakaevamirhan@gmail.com>
2026-06-01 14:32:07 +0300

    ДЗ на прошлое занятие

--- homework.py
+++ homework.py
@@ -1,1 +1,19 @@
-print("w")
+#1
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print(f"сумма:{a + b + c}")
+#print(f"произведение:{a * b * c}")
+#2
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print(a-b-c)
+#3
+#a = int(input())
+#b = int(input())
+#print(a*b/2)
+#4
+#print("To be\nor not\nto be")
+#5
+#print("“Life is what happens\nwhen\nyou’re busy making other plans”\nJohn Lennon.")
