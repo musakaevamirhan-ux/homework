5e94c8107b0dfacf1fd4a9936a587473c6568f83
musakaevamirhan-ux <musakaevamirhan@gmail.com>
2026-06-12 19:44:37 +0300

    ДЗ 4

---
+++ dz4.py
@@ -0,0 +1,55 @@
+# 1
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     a, b = b, a
+# sum = 0
+# for i in range(a, b+1):
+#     sum += i
+# print(sum)
+# 2
+# a = int(input())
+# b = int(input())
+# max = 1
+# for i in range(1,a+1):
+#     if a%i == 0 and b%i == 0:
+#         max = i
+# print(max)
+# 3
+# a = int(input())
+# for i in range(1,a+1):
+#     if a%i==0:
+#         print(i, end=" ")
+# 4
+# a = int(input())
+# cnt = 0
+# for i in range(1,a+1):
+#     k = 10 ** i
+#     cnt += 1
+#     if a//k == 0:
+#         break
+# print(cnt)
+# 5
+# zero = 0
+# plus = 0
+# minus = 0
+# chet = 0
+# nechet = 0
+# for i in range(10):
+#     b = int(input())
+#     if b == 0:
+#         zero += 1
+#     if b > 0:
+#         plus += 1
+#     if b < 0:
+#         minus += 1
+#     if b%2 != 0:
+#         nechet += 1
+#     if b%2 == 0:
+#         chet += 1
+# print(zero)
+# print(plus)
+# print(minus)
+# print(nechet)
+# print(chet)
+
