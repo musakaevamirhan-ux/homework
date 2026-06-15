cbc9ec7b1ecadebfc6efa1105c32dece12bfbfee
musakaevamirhan-ux <musakaevamirhan@gmail.com>
2026-06-15 19:02:46 +0300

    ДЗ 4

--- dz4.py
+++ dz4.py
@@ -8,13 +8,16 @@
 #     sum += i
 # print(sum)
 # 2
-# a = int(input())
-# b = int(input())
+# a = abs(int(input()))
+# b = abs(int(input()))
 # max = 1
-# for i in range(1,a+1):
+# if a > b:
+#     a, b = b, a
+# for i in range(a, 0, -1):
 #     if a%i == 0 and b%i == 0:
 #         max = i
-# print(max)
+#         print(max)
+#         break
 # 3
 # a = int(input())
 # for i in range(1,a+1):
